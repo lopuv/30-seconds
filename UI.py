0988ed040e701916289ac2152a987af1d19d2da8
lopuv <400917@vistacollege.nl>
2022-06-18 00:01:26 +0200

    quiz brain

--- UI.py
+++ UI.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter.ttk import Label
 import pandas
+import time
 
 
 class UI(Tk):
@@ -25,9 +26,11 @@ class UI(Tk):
         self.tekst = "hello"
         self.btn = Button(self, text="Start", bg=self.SETUP["blue_sapphire"], fg=self.SETUP["isabelline"],
                           width=15, height=2, command=lambda *args: self.clear(1))
+        self.a = 0
+        self.a2 = 0
         self.answer = []
+        self.score = 0
         self.data = pandas.read_csv("question.csv")
-
         self.mainscreen()
 
     def lable(self, tekst, fg_color, bg_color, y, x):
@@ -57,41 +60,48 @@ class UI(Tk):
         elif number == 4:
             self.wrong_answer()
 
-    def check_answer(self, answer):
-        for a in answer:
-            if self.data["right_answer"].all() == a.all():
-                print("hello")
-                break
+    def check_answer(self):
+        d = self.data["right_answer"][self.a2]
+        opt = self.opt_sel.get()
+
+        if int(d) == opt:
+            self.score += 10
+            self.clear(3)
+        else:
+            self.score -= 10
+            self.clear(4)
+
+        # if print(int(d) == opt):
+        #     self.score += 10
+        #     self.clear(3)  # right answer
+        # else:
+        #     self.score -= 10
+        #     self.clear(4)  # wrong answer
 
     def answers(self):
-        # look first for right position to create the buttons
-        # answer = ["antwoord A", "antwoord B", "antwoord C", "antwoord D"]
-        a = ["answer_a", "answer_b", "answer_c", "answer_d"]
+        options = ["answer_a", "answer_b", "answer_c", "answer_d"]
+        self.configure(bg=self.SETUP["pewter_blue"])
 
-        answer_a = self.data[a[0]]
-        answer_b = self.data[a[1]]
-        answer_c = self.data[a[2]]
-        answer_d = self.data[a[3]]
+        for a in range(0, 4):
+            answer = self.data[options[a]]
+            self.answer.append(answer)
 
-        self.answer.append(answer_a)
-        self.answer.append(answer_b)
-        self.answer.append(answer_c)
-        self.answer.append(answer_d)
-
-        b = 0
         y = 230
         x = 150
         q_list = []
-        for _ in a:
-            btn = Radiobutton(self, text=self.answer[b], bg=self.SETUP["blue_sapphire"], fg=self.SETUP["isabelline"],
-                              width=15, height=2, variable=self.opt_sel, value=len(q_list) + 1)  # adjust this number
+        t = 0
+        for _ in options:
+            btn = Radiobutton(self, text=self.answer[self.a][self.a2], bg=self.SETUP["blue_sapphire"],
+                              fg=self.SETUP["isabelline"], width=15, height=2, variable=self.opt_sel,
+                              value= len(q_list)+1, indicatoron=False, command=self.check_answer)  # adjust this number
+            btn.deselect()
             btn.place(y=y, x=x)
+            self.a += 1
+            t += 1
             q_list.append(btn)
-            b += 1
             x -= 150
-            print(x)
-            if b == 4:
-                b = 0
+            if t == 4:
+                t = 0
             if x == -150:
                 x = 150
                 y -= 200
@@ -102,14 +112,30 @@ class UI(Tk):
 
         # do a self.clear(3) to trigger the right_answer function
 
+    def ref(self):
+        self.update()
+        time.sleep(5)
+        self.a2 += 1
+        self.clear(1)
+
     def right_answer(self):
         self.configure(bg=self.SETUP["green"])
         self.tekst = "Gefeliciteerd de vraag is goed"
         self.lable(tekst=self.tekst, fg_color=self.SETUP["isabelline"],
                    bg_color=self.SETUP["green"], y=200, x=200)
+        self.tekst = f"je huidige score is {self.score}"
+        self.lable(tekst=self.tekst, fg_color=self.SETUP["isabelline"],
+                   bg_color=self.SETUP["green"], y=100, x=100)
+        # call to a timer method so it can go to the next question self.a needs a random function so the rest of the
+        # answer function will work and conintue the quiz
+        self.ref()
 
     def wrong_answer(self):
         self.configure(bg=self.SETUP["red"])
         self.tekst = "Helaas de vraag is fout volgende keer beter"
         self.lable(tekst=self.tekst, fg_color=self.SETUP["isabelline"],
                    bg_color=self.SETUP["red"], y=200, x=200)
+        self.tekst = f"je huidige score is {self.score}"
+        self.lable(tekst=self.tekst, fg_color=self.SETUP["isabelline"],
+                   bg_color=self.SETUP["red"], y=100, x=100)
+        self.ref()
--- notes.py
+++ notes.py
@@ -1,22 +1,4 @@
-import pandas
-
-a = ["answer_a", "answer_b", "answer_c", "answer_d"]
-# get csv file and print it to the screen in a prettytable
-
-answers = []
-
-data = pandas.read_csv("question.csv")
-
-answer_a = data[a[0]]
-answer_b = data[a[1]]
-answer_c = data[a[2]]
-answer_d = data[a[3]]
-
-answers.append(answer_a)
-answers.append(answer_b)
-answers.append(answer_c)
-answers.append(answer_d)
-
+# END OF THE PROGRAM
 for right in answers:
     print(right)
     if data["right_answer"].all() == right.all():
